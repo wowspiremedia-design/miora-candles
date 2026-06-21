e66b20c335a7ddad817356734fd352b4a3e5f68e
Rahul Mukherjee <wowspiremedia@gmail.com>
2026-06-22 03:15:00 +0530

    Fix Sanity CDN and design updates

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -4,6 +4,6 @@ export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '3p9konjs',
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-01-01',
-  useCdn: true,
+  useCdn: false,
   token: process.env.SANITY_API_READ_TOKEN,
 })
